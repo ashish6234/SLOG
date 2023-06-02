e4562bb7a9cd7baa4ba1c89ac4984ad67390a1ea
ashish6234 <s.ashish62.aa@gmail.com>
2023-06-02 23:51:06 +0530

    version 5 added some new js command in array and do while loop and there will be some changes in gpn team project

---
+++ javascript/eightDoWhile.js
@@ -0,0 +1,18 @@
+// while loop
+let i = 0;
+while(i <= 9){
+    console.log(i);
+    i++;
+}
+
+
+// Do while Loop
+
+let a = 10;
+do{
+    console.log(a);
+    a++;
+
+}
+while(a <= 9);
+console.log("value of a is", a)
