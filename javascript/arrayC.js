b6ff3fbee746a4c54eea843d76ebe9fcfcfeb3c6
ashish6234 <s.ashish62.aa@gmail.com>
2023-07-01 19:00:56 +0530

    V-1 Added some js code in js file

---
+++ javascript/arrayC.js
@@ -0,0 +1,78 @@
+// -Arrays 
+
+
+// const groceries = ['pears', 'apple', 'banana', 'oranges']
+// console.log(groceries)
+
+
+// ? Arrays Methods push 
+// groceries.push('cookies')
+// groceries.push('grapes')
+// groceries.push('mango')
+// groceries.push('fruits')
+// groceries.push('veg')
+// console.log(groceries)
+
+
+// ? Arrays Methods slice 
+// * start from 1 inclusive and upto 3
+// console.log(groceries.slice(0, 9))
+// console.log(groceries.slice(1, 4))
+// console.log(groceries.slice(3, 6))
+// console.log(groceries.slice(0, 1))
+
+
+// ? arrays method indexof 
+
+// console.log(groceries.indexOf('grapes'))
+
+// console.log(groceries.indexOf('pears'))
+
+// console.log(groceries.indexOf('veg'))
+
+//? arrays method length
+
+// console.log(groceries.length)
+
+
+
+
+//- Objects in Js 
+
+const person = {
+    firstName: 'leonardo',
+    dress: 'blazzer'
+}
+
+//? Dot notation in Objects
+console.log(person.firstName)
+console.log(person.dress)
+
+
+//? bracket notation in object
+console.log(person['dress'])
+console.log(person['firstName'])
+
+
+//? Assign in objects
+
+person.numB = '7985038855'
+person['numB'] = '9383900222'
+console.log(person)
+
+
+//= another example 
+
+let perS = {
+    firsTN: 'Ashish',
+    dresS: 't-shirt',
+    nmB: '9330003000'
+}
+
+console.log(perS)
+console.log(perS.firsTN)
+
+
+//- Function in Js with 2 arguments es6 arrow function
+
+//? using object and template litrals
